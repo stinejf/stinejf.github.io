77962cb90e1b30dec65b386663a4f47068481d49
stinejf <stine.foyen@skye.no>
2020-11-18 13:10:08 +0100

    Added files and folders

---
+++ resources/js/quiz.js
@@ -0,0 +1,9 @@
+function startQuiz() {
+    var svar = prompt("Hvem skrev 'Et dukkehjem'?"); 
+    if (svar === "Henrik Ibsen" || svar === "Ibsen" || svar === "henrik ibsen" || svar === "ibsen") {
+       alert("RIKTIG!"); 
+    }
+    else {
+        alert("Feil!"); 
+    }
+}
