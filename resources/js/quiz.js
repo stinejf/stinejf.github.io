246dcf8f5e6850dfc2cd16bd12733721132de6a3
stinejf <stine.foyen@skye.no>
2020-11-18 13:13:44 +0100

    Added files and folders

--- resources/js/quiz.js
+++ resources/js/quiz.js
@@ -1,3 +1,5 @@
+/* KODE HENTET FRA BOKEN KODE 2 INFORMASJONSTEKNOLOGI AV CAPPELEN DAMM */
+
 function startQuiz() {
     var svar = prompt("Hvem skrev 'Et dukkehjem'?"); 
     if (svar === "Henrik Ibsen" || svar === "Ibsen" || svar === "henrik ibsen" || svar === "ibsen") {
