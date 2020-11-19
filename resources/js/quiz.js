280bea0b6444d1487028cc48c6911c20c41a61fd
Stine Jonassen Føyen <Stine.Jonassen.Foyen@vlfk.no>
2020-11-19 09:51:26 +0100

    Test

---
+++ resources/js/quiz.js
@@ -0,0 +1,8 @@
+var svar = prompt("Hvem skrev et dukkehjem?"); 
+if (svar === "Henrik Ibsen" || svar === "Ibsen") {
+    alert("Riktig! "); 
+}
+else {
+    alert("Beklager, feil svar!"); 
+}
+document.getElementById("svar"); 
