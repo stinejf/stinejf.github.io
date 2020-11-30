d084b3260112f7c9860a29e87c83c30f58d00640
Stine Jonassen Føyen <Stine.Jonassen.Foyen@vlfk.no>
2020-11-30 14:46:16 +0100

    Changed image links

--- resources/js/yatzy.js
+++ resources/js/yatzy.js
@@ -5,7 +5,7 @@ button.onclick = function() {
     var results = []; 
     for (var i=1; i<=5; i++) {
         var dice = parseInt(Math.random()*6+1); 
-        document.getElementById("dice"+i).src = "resources/images/dice"+dice+".png"; 
+        document.getElementById("dice"+i).src = "../resources/images/dice"+dice+".png"; 
         results += dice; 
     }
     if (results[0] == results[1] == results[2] == results[3] == results[4])
